9d0b627023fae18ec3ebd755bb49c35248283f29
elif4423 <15290063@firat.edu.tr>
2022-11-19 13:29:21 +0300

    Add project files.

---
+++ Business/Concrete/PttMenager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttMenager:ISupplierService
+    {
+        private IapplicantService _applicantService;
+        public PttMenager(IapplicantService applicantService)
+        { // constructor new yapıldığında çalışır
+
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            PersonMenager personMenager = new PersonMenager();
+            if(_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine ("MASKE VERİLMEDİ");   
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Person person1 = new Person();
+            person1.FirstName = "Elif";
+            person1.LastName = "GÜNGÖRDÜ";
+            person1.DateOfBirthYear = 1995;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+
+            PttMenager pttMenager = new PttMenager(new PersonMenager());
+            pttMenager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
